cd97a4ca91dfe49f61e4407f2516f2a5843f52a2
gatik-droid <gatikadlakha1234@gmail.com>
2021-09-24 15:01:18 +0530

    i did this

--- Chain.js
+++ Chain.js
@@ -1,18 +1,19 @@
 class Chain{
-    constructor(bodyA, bodyB){
+    constructor(bodyA, pointB){
         var options = {
             bodyA: bodyA,
-            bodyB: bodyB,
+            pointB: pointB,
             stiffness: 0.04,
             length: 10
         }
+        this.pointB = pointB
         this.chain = Constraint.create(options);
         World.add(world, this.chain);
     }
 
     display(){
         var pointA = this.chain.bodyA.position;
-        var pointB = this.chain.bodyB.position;
+        var pointB = this.pointB;
         strokeWeight(4);
         line(pointA.x, pointA.y, pointB.x, pointB.y);
     }
